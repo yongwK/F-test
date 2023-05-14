079464c68448d6118bd8f1912be0ee3754379129
malrang <nsrkyw@gmail.com>
2023-05-14 18:12:44 +0900

    branch printFC  commit localRepository

---
+++ calculator/src/main/java/tool/made/calculator/PrintFC.java
@@ -0,0 +1,14 @@
+package tool.made.calculator;
+
+public class PrintFC {
+
+	public String print(String str) {
+		String rtnStr = "";
+		
+		if(str instanceof String) {
+			rtnStr = str;
+		}
+		
+		return rtnStr;
+	}
+}
